dd842e8c5edaca8e377e5b00a8330e8c5c7e9d11
canknbr <cankanbur1@gmail.com>
2023-07-20 19:59:18 +0300

    added

--- src/components/Navbar.js
+++
@@ -1,9 +0,0 @@
-import React, { useState } from "react";
-import Search from "./Search";
-import Logo from "./Logo";
-import NumResult from "./NumResult";
-const Navbar = ({ children }) => {
-  return <nav className="nav-bar">{children}</nav>;
-};
-
-export default Navbar;
--- src/components/NumResult.js
+++
@@ -1,9 +0,0 @@
-const NumResult = ({ movies }) => {
-  return (
-    <p className="num-results">
-      Found <strong>{movies.length}</strong> results
-    </p>
-  );
-};
-
-export default NumResult;
